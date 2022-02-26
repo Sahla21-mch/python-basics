479e95d019c25bc5fb536a801a780d8b4bb651a3
Sahla21-mch <you@example.com>
2022-02-26 20:01:02 +0100

    trouble

---
+++ theningers1.py
@@ -0,0 +1,14 @@
+def is_miltiple(n,m):
+    if m % n == 0:
+        return True
+    else:
+        return False
+
+
+n = int(input("enter the number :"))
+m = int(input("enter the multiple :"))
+print(is_miltiple(n,m))
+    
+
+
+
