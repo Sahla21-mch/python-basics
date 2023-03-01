77d60c22ba46114a2e959b6651ffa5b6db88aa1d
Sahla21-mch <99275493+Sahla21-mch@users.noreply.github.com>
2023-03-01 14:41:45 +0100

    Update dictionaries.py

--- dictionaries.py
+++ dictionaries.py
@@ -8,3 +8,14 @@ my_list = {
 print (my_list["maggi"])
 print (len(my_list))
 print (type(my_list))
+
+#another methos of declaring a dictionary 
+my_list2 = dict(
+    tomatoes = "200frs",
+    onion = "100frs",
+    green_spicies ="50frs",
+    rice ="900frs",
+    maggi ="50frs",
+    salt = "50frs"
+)
+print (my_list2)
