fcb92507328f9b9a4d487783e576c0df729c8a34
Sahla21-mch <99275493+Sahla21-mch@users.noreply.github.com>
2023-03-02 03:07:46 +0100

    Update dictionaries.py

--- dictionaries.py
+++ dictionaries.py
@@ -25,3 +25,38 @@ print (this)
 this = my_list2.values()
 this = my_list2.keys()
 print (this)
+
+my_list = {
+    "tomatoes": "200frs",
+    "onion": "100frs",
+    "green_spicies": "50frs",
+    "rice": "900frs",
+    "maggi": "50frs",
+    "salt": "50frs"}
+my_list.update({"oil": "1500"})
+print(my_list)
+print (my_list["maggi"])
+print (len(my_list))
+print (type(my_list))
+
+#another methos of declaring a dictionary 
+my_list2 = dict(
+    tomatoes = "200frs",
+    onion = "100frs",
+    green_spicies ="50frs",
+    rice ="900frs",
+    maggi ="50frs",
+    salt = "50frs"
+)
+my_list2["rice"] = 1200
+print (my_list2)
+this = my_list2.values()
+this = my_list2.keys()
+print (this)
+tuples = my_list2.items()
+print (tuples)
+if "magg" in my_list2:
+    print("yes, there's maggi")
+else:
+    print("NO! doesn't exist")
+
